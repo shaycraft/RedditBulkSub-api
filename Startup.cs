d0b1cb0e076306c52e5eb33eb038d34f8421389b
Sam Haycraft <phreakdawg@gmail.com>
2018-11-06 17:13:18 -0700

    adds cors support

--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,7 @@ namespace RedditBulkSub_api
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.Add(new ServiceDescriptor(typeof(ConfigService), _configService));
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +44,14 @@ namespace RedditBulkSub_api
             {
                 app.UseHsts();
             }
+
+            // enable CORS
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin();
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+            });
 
             // app.UseHttpsRedirection();
             app.UseMvc();
